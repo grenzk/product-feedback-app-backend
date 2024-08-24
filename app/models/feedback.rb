27b77b062c218a01aa47aea51524c8f85f5208b6
grenzk <74033991+grenzk@users.noreply.github.com>
2024-08-24 16:18:48 +0800

    Add model validations for feedback

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,2 +1,7 @@
 class Feedback < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :category, presence: true
+  validates :detail, presence: true, length: { maximum: 100 }
+
+  enum :category, [ :ui, :ux, :enhancement, :bug ]
 end
