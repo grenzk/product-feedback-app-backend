c49f8a72c1cc2ca90cfdda03f84e8c06b463e76e
grenzk <74033991+grenzk@users.noreply.github.com>
2024-08-24 17:57:35 +0800

    Associate feedback with comments

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,4 +1,6 @@
 class Feedback < ApplicationRecord
+  has_many :feedbacks
+
   validates :title, presence: true, length: { maximum: 30 }
   validates :category, presence: true
   validates :detail, presence: true, length: { maximum: 100 }
